4be063e85214941af8c33dedd75c8e49d1fb5ff6
shubhamshinde17 <shubhamshinde266@gmail.com>
2026-01-27 23:53:16 +0530

    Initialize Spring Boot API project with Gradle setup, Docker Compose configuration, and basic application structure

---
+++ src/test/java/com/home_cloud/api/TestApiApplication.java
@@ -0,0 +1,11 @@
+package com.home_cloud.api;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
